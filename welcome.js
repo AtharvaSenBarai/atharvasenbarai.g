d89702112cc9416146ba0660310cc18aaed4e85c
Atharva Sen <atharvasenbarai@gmail.com>
2021-12-18 09:09:54 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -6,6 +6,16 @@ firebase.auth().onAuthStateChanged((user)=>{
     }
 })
 
+const userLoggedIn = (user) => {
+    formContainer.remove()
+    $('#auth-container').removeClass('d-none')
+    authText.innerText = `You are Logged In as ${user.email}\nVerified: ${user.emailVerified}`
+    if (user.emailVerified) {
+        verifyEmailBtn.classList.add('d-none')
+    } else {
+        verifyEmailBtn.classList.remove('d-none')
+    }
+}
 
 function logout(){
     firebase.auth().signOut()
